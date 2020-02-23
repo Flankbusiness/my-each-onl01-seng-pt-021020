42b8e364f48a9b73e56cef6e72dfe485181d15fe
Steven Prindle <flankbusiness@gmail.com>
2020-02-23 03:24:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
 #def my_each # put argument(s) here
   # code here
 #end
+
+def my_each # put argument(s) here
+  # code here
+def my_each(collection = ["me", "oh", "my"])
+  my_each(collection) do |i|
+    puts i
+end 
